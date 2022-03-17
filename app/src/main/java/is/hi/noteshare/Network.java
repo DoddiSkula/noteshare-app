4682f53f29f5c6d7e48fa80124ad700edb7de7d1
Tht40 <77277019+Tht40@users.noreply.github.com>
2022-03-17 13:58:04 +0000

    network
    dummy files

--- app/src/main/java/is/hi/noteshare/Network.java
+++ app/src/main/java/is/hi/noteshare/Network.java
@@ -1,15 +1,29 @@
 package is.hi.noteshare;
 
 
+import is.hi.noteshare.service.CourseService;
 
-public class Network {
-    /*
+public class Network implements CourseService {
+/*
     getCourses() {
         // return list of dummy courses
-        return [
-        new Course(a,a,a),
-        new Course(b,b,b)
-        ]
+        return List<
+        new Course(1 ,HBV401G, Þróun hugbúnaðar, HÍ),
+        new Course(2, HBV501G, Hugbúnaðarverkefni 1, HÍ),
+        new Course(3, HBV601G, Hugbúnaðarverkefni 2, HÍ),
+        new Course(4, TÖL101G, Tölvunarfræði 1, HÍ),
+        new Course(5, TÖL107G, Vefforritun 1, HÍ),
+        new Course(6, HBV201G, Viðmótsforritun, HÍ),
+        new Course(7, TÖL104G, Stærðfræðimynstur, HÍ),
+        new Course(8, TÖL203G, Tölvunarfræði 2, HÍ),
+        new Course(9, TÖL401G, Stýrikerfi, HÍ),
+        new Course(10, TÖL304G, Forritunarmál, HÍ)
+        >
+    };
+
+    getUsers() {
+        return List< new User(1, admin, admin@admin.com, 123)
+        >
     };
 
     getCourses(name: String);
