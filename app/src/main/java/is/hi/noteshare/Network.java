38f681925dc678cf6a789776dc18d590f647950f
Doddi <doddiskula@gmail.com>
2022-03-20 15:50:30 +0000

    fix network class

--- app/src/main/java/is/hi/noteshare/Network.java
+++ app/src/main/java/is/hi/noteshare/Network.java
@@ -11,22 +11,24 @@ public class Network {
 
     public List<Course> getCourses() {
         // return list of dummy courses
-        return new ArrayList<Course>(
-                new Course(1 ,"HBV401G", "Þróun hugbúnaðar", "HÍ"),
-                new Course(2, "HBV501G", "Hugbúnaðarverkefni 1", "HÍ"),
-                new Course(3, "HBV601G", "Hugbúnaðarverkefni 2", "HÍ"),
-                new Course(4, "TÖL101G", "Tölvunarfræði 1", "HÍ"),
-                new Course(5, "TÖL107G", "Vefforritun 1", "HÍ"),
-                new Course(6, "HBV201G", "Viðmótsforritun", "HÍ"),
-                new Course(7, "TÖL104G", "Stærðfræðimynstur", "HÍ"),
-                new Course(8, "TÖL203G", "Tölvunarfræði 2", "HÍ"),
-                new Course(9, "TÖL401G", "Stýrikerfi", "HÍ"),
-                new Course(10, "TÖL304G", "Forritunarmál", "HÍ")
-                );
+        List<Course> mockCourses = new ArrayList<>();
+
+        mockCourses.add(new Course(1 ,"HBV401G", "Þróun hugbúnaðar", "HÍ"));
+        mockCourses.add(new Course(2, "HBV501G", "Hugbúnaðarverkefni 1", "HÍ"));
+        mockCourses.add(new Course(3, "HBV601G", "Hugbúnaðarverkefni 2", "HÍ"));
+        mockCourses.add(new Course(4, "TÖL101G", "Tölvunarfræði 1", "HÍ"));
+        mockCourses.add(new Course(5, "TÖL107G", "Vefforritun 1", "HÍ"));
+        mockCourses.add(new Course(6, "HBV201G", "Viðmótsforritun", "HÍ"));
+        mockCourses.add(new Course(7, "TÖL104G", "Stærðfræðimynstur", "HÍ"));
+        mockCourses.add(new Course(8, "TÖL203G", "Tölvunarfræði 2", "HÍ"));
+        mockCourses.add(new Course(9, "TÖL401G", "Stýrikerfi", "HÍ"));
+        mockCourses.add(new Course(10, "TÖL304G", "Forritunarmál", "HÍ"));
+
+        return mockCourses;
     };
 
     public User getUser() {
-        return new User(1, "admin", "admin@admin.com", 123);
+        return new User(1, "admin", "admin@admin.com", "123");
     };
     
     /*
