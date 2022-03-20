71740963d1444216eaf34aea9007e41e42803c52
Rögnvaldur Bjarnason <56052147+RoggoDude@users.noreply.github.com>
2022-03-20 15:23:38 +0000

    Entities set up

--- app/src/main/java/is/hi/noteshare/data/models/Course.java
+++ app/src/main/java/is/hi/noteshare/data/models/Course.java
@@ -1,4 +1,36 @@
 package is.hi.noteshare.data.models;
 
 public class Course {
+    private int id;
+    private String shortName;
+    private String longName;
+    private String schoolName;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getLongName() {
+        return longName;
+    }
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
 }
--- app/src/main/java/is/hi/noteshare/data/models/File.java
+++ app/src/main/java/is/hi/noteshare/data/models/File.java
@@ -1,4 +1,45 @@
 package is.hi.noteshare.data.models;
+import java.util.Date;
 
 public class File {
+    private int id;
+    private Date date;
+    private String title;
+    private String description;
+    private String fileType;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
 }
