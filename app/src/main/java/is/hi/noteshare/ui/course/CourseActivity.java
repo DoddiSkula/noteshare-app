82a55a7bfde2d35afedf8c3189552c258fe834b7
Doddi <doddiskula@gmail.com>
2022-04-20 16:52:37 +0000

    select file

--- app/src/main/java/is/hi/noteshare/ui/course/CourseActivity.java
+++ app/src/main/java/is/hi/noteshare/ui/course/CourseActivity.java
@@ -2,9 +2,17 @@ package is.hi.noteshare.ui.course;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import is.hi.noteshare.R;
+import is.hi.noteshare.ui.login.LoginActivity;
+import is.hi.noteshare.ui.main.MainActivity;
+import is.hi.noteshare.ui.upload.UploadActivity;
 
 public class CourseActivity extends AppCompatActivity {
 
@@ -12,5 +20,14 @@ public class CourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.floatingAddButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CourseActivity.this, UploadActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/is/hi/noteshare/ui/upload/UploadActivity.java
+++ app/src/main/java/is/hi/noteshare/ui/upload/UploadActivity.java
@@ -1,16 +1,73 @@
 package is.hi.noteshare.ui.upload;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
 
 import is.hi.noteshare.R;
 
 public class UploadActivity extends AppCompatActivity {
 
+    ActivityResultLauncher<Intent> resultLauncher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        // Initialize result launcher
+        resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        // Initialize result data
+                        Intent data = result.getData();
+                        // check condition
+                        if (data != null) {
+                            // Get PDf uri
+                            Uri sUri = data.getData();
+                            // Get PDF path
+                            String sPath = sUri.getPath();
+
+                            Log.e("data", String.valueOf(data));
+                            Log.e("sUri", String.valueOf(sUri.getLastPathSegment()));
+                            Log.e("sPath", String.valueOf(sPath));
+                            File file = new File(sUri.getPath());
+                            Log.e("file name", file.getName());
+                        }
+                    }
+                });
+
+        Button selectButton = (Button) findViewById(R.id.selectFileButton);
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("application/pdf");
+                resultLauncher.launch(intent);
+            }
+        });
+
+        Button uploadButton = (Button) findViewById(R.id.uploadButton);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+
 }
